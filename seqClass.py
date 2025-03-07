0677998c5830a87623ce5dab69daefdce46ffe47
Richard D Shipman <djshipman2011@gmail.com>
2025-03-07 14:00:40 +0100

    Fix the seqClass.py script so that it is able to classify correctly any RNA or DNA sequence.

--- seqClass.py
+++ seqClass.py
@@ -1,36 +1,35 @@
 #!/usr/bin/env python
 
-import sys, re
+import sys
+import re
 from argparse import ArgumentParser
 
-parser = ArgumentParser(description = 'Classify a sequence as DNA or RNA')
-parser.add_argument("-s", "--seq", type = str, required = True, help = "Input sequence")
-parser.add_argument("-m", "--motif", type = str, required = False, help = "Motif")
+# Argument parser setup
+parser = ArgumentParser(description='Classify a sequence as DNA or RNA')
+parser.add_argument("-s", "--seq", type=str, required=True, help="Input sequence")
+parser.add_argument("-m", "--motif", type=str, required=False, help="Motif")
 
-
+# Print help if no arguments are provided
 if len(sys.argv) == 1:
     parser.print_help()
     sys.exit(1)
 
+# Parse arguments
 args = parser.parse_args()
+args.seq = args.seq.upper()  # Convert sequence to uppercase
 
-
-args.seq = args.seq.upper()                 # Note we just added this line
-
-
-if re.search('^[ACGTU]+$', args.seq):
-    if re.search('T', args.seq):
-        print ('The sequence is DNA')
-    elif re.search('U', args.seq):
-        print ('The sequence is RNA')
-    else:
-        print ('The sequence can be DNA or RNA')
+# Validate and classify sequence
+if re.fullmatch(r'^[ACGT]+$', args.seq):  # Only A, C, G, T allowed
+    print("The sequence is DNA")
+elif re.fullmatch(r'^[ACGU]+$', args.seq):  # Only A, C, G, U allowed
+    print("The sequence is RNA")
 else:
-    print ('The sequence is not DNA nor RNA')
+    print("The sequence is not DNA nor RNA")
 
+# Motif search
 if args.motif:
     args.motif = args.motif.upper()
-    print(f'Motif search enabled: looking for motif "{args.motif}" in sequence "{args.seq}"... ', end = '')
+    print(f'Motif search enabled: looking for motif "{args.motif}" in sequence "{args.seq}"... ', end='')
     if re.search(args.motif, args.seq):
         print("FOUND")
     else:
